5e06d1da3db06b2d7f6b8b91a139faff96303acd
Matthew Conlen <mc@mathisonian.com>
2011-11-11 16:24:49 -0500

    Added reply toggling.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,34 +1,34 @@
 
-function displayThreads(msg) {
-	var arr = eval("(" + msg + ")");
-	$("#threadDiv").empty();
-	for(var i = 0; i < arr.length; i++) {
-		var obj = arr[i];
-		$("#threadDiv").append("<div class='row thread' id='thread" + obj["t_id"] + "'></div>");
-		$("#thread" + obj["t_id"]).append("<div class='three columns'><img src='http://placehold.it/100x100'></div>");
-		$("#thread" + obj["t_id"]).append("<div class='eight columns'><p>" + obj["content"] + "</p></div>");
-        $("#thread" + obj["t_id"]).append("<div class='one columns'><div class='numResponses'><h1>" + obj["response_count"] + "</h1></div></div>");
-    }
-	return false;
-}
-
-function getThreadsOnLoad() {
-	$.ajax({
-           type : "get",
-           url : "./php/getThreads.php",
-           cache : false,
-           async : true,
-           data : {
-           "s" : "new",
-           "n" : 10
-           },
-           success : function(msg) {
-           displayThreads(msg);
-           },
-           complete : function() {
-           }
-           });
-}
+//function displayThreads(msg) {
+//	var arr = eval("(" + msg + ")");
+//	$("#threadDiv").empty();
+//	for(var i = 0; i < arr.length; i++) {
+//		var obj = arr[i];
+//		$("#threadDiv").append("<div class='row thread' id='thread" + obj["t_id"] + "'></div>");
+//		$("#thread" + obj["t_id"]).append("<div class='three columns'><img src='http://placehold.it/100x100'></div>");
+//		$("#thread" + obj["t_id"]).append("<div class='eight columns'><p>" + obj["content"] + "</p></div>");
+//        $("#thread" + obj["t_id"]).append("<div class='one columns'><div class='numResponses'><h1>" + obj["response_count"] + "</h1></div></div>");
+//    }
+//	return false;
+//}
+//
+//function getThreadsOnLoad() {
+//	$.ajax({
+//           type : "get",
+//           url : "./php/getThreads.php",
+//           cache : false,
+//           async : true,
+//           data : {
+//           "s" : "new",
+//           "n" : 10
+//           },
+//           success : function(msg) {
+//           displayThreads(msg);
+//           },
+//           complete : function() {
+//           }
+//           });
+//}
 
 
 $(document).ready(function() {
@@ -41,5 +41,21 @@ $(document).ready(function() {
                   /* Remove this and jquery.placeholder.min.js if you don't need :) */
                   
                   $('input, textarea').placeholder();
-                  getThreadsOnLoad();
-                  });
+//                  getThreadsOnLoad();
+
+                  $(".thread").click(function () {
+
+                        if($(this).hasClass("thread")) { 
+                            $(".threadSelected").toggleClass("thread threadSelected");
+                            $(this).toggleClass("thread threadSelected"); 
+                            if(!$('#responseDiv').is(":visible")) {
+                                $('#responseDiv').slideToggle();
+                            }
+                        } else {
+                            $('#responseDiv').slideToggle();
+                            $(".threadSelected").toggleClass("thread threadSelected");
+                        }
+                                     
+                    });
+                  
+});
